aeec9023cf53598a10a1f4cdfea98b7b92c3836d
邓兴建 <2796541963@qq.com>
2026-06-03 09:00:42 +0800

    数据预处理及BERT模型

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import torch
 import os
+from transformers.models import BertModel,BertTokenizer,BertConfig
+
 
 class Config(object):
     def __init__(self):
@@ -8,4 +10,19 @@ class Config(object):
         self.train_path = self.data_path + "/train.txt"
         self.test_path = self.data_path + "/test.txt"
         self.val_path = self.data_path + "/val.txt"
-        self.orig_data_path = self.data_path +"/manufacturing_repair_text_dataset_cn.txt"
+        self.fault_class_path = self.data_path + "/fault_class.txt"
+        self.risk_grad_class_path= self.data_path + "/risk_grad_class.txt"
+        self.department_class_path= self.data_path + "/department_class_.txt"
+
+
+
+        self.orig_data_path = self.data_path +"/manufacturing_repair_text_dataset_cn(2).txt"
+        self.num_epochs = 1  # epoch数
+        self.batch_size = 256  # mini-batch大小
+        self.pad_size = 32  # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5  # 学习率
+        self.bert_path = "./bert-base-chinese"  # 预训练BERT模型的路径
+        self.bert_model = BertModel.from_pretrained(self.bert_path)
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)  # BERT模型的分词器
+        self.bert_config = BertConfig.from_pretrained(self.bert_path)  # BERT模型的配置
+        self.hidden_size = 768
--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
-import pandas
 import torch
+from torch.utils.data import DataLoader
 from config import Config
 import json
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 config = Config()
 
-
+# 处理原始数据，生成标签文档，返回基础处理数据
 def load_raw_data():
     data = []
     fault_set = set()
@@ -42,9 +44,8 @@ def load_raw_data():
     with open(config.department_class_path,'w',encoding='utf-8') as f:
         for idx,name in id_to_department.items():
             f.write(f"{idx}\t{name}\n")
-
     return data
-
+# 进行id到标签，标签到id的映射
 def id2class():
     fault_to_id ={}
     risk_grad_to_id ={}
@@ -56,41 +57,46 @@ def id2class():
         for line in f:
             line = line.strip()
             parts  = line.split("\t")
-            fault_to_id[parts[1]] = parts[0]
-            id_to_fault[parts[0]] = parts[0]
+            fault_to_id[parts[1]] = int(parts[0])
+            id_to_fault[int(parts[0])] = parts[1]
     with open(config.risk_grad_class_path,'r',encoding='utf-8') as f:
         for line in f:
             line = line.strip()
             parts  = line.split("\t")
-            risk_grad_to_id[parts[1]] = parts[0]
-            id_to_risk_grad[parts[0]] = parts[1]
+            risk_grad_to_id[parts[1]] = int(parts[0])
+            id_to_risk_grad[int(parts[0])] = parts[1]
     with open(config.department_class_path,'r',encoding='utf-8') as f:
         for line in f:
             line = line.strip()
             parts  = line.split("\t")
-            department_to_id[parts[1]] = parts[0]
-            id_to_department[parts[0]] = parts[1]
+            department_to_id[parts[1]] = int(parts[0])
+            id_to_department[int(parts[0])] = parts[1]
     # print(fault_dict)
     # print(risk_grad_dict)
     # print(department_dict)
     return fault_to_id,id_to_fault,risk_grad_to_id,id_to_risk_grad,department_to_id,id_to_department
-
+# 将标签映射为0.1.2....的数字
 def dataset():
-    fault_to_id, id_to_fault, risk_grad_to_id, id_to_risk_grad, department_to_id, id_to_department= id2class()
     data = load_raw_data()
+    fault_to_id, id_to_fault, risk_grad_to_id, id_to_risk_grad, department_to_id, id_to_department= id2class()
     data = pd.DataFrame(data,columns=['text','fault','risk_grad','department'])
     data['fault'] = data['fault'].map(fault_to_id)
     data['risk_grad'] = data['risk_grad'].map(risk_grad_to_id)
     data['department'] = data['department'].map(department_to_id)
     print(data.head())
     return data
-
+# 完成bert的特征编码
 def collate_fn(batch):
     texts = [item[0] for item in batch]
     fault = [item[1] for item in batch]
     risk_grad = [item[2] for item in batch]
     department = [item[3] for item in batch]
-    text_tokens = config.tokenizer.batch_encode_plus(texts,padding=True)
+    text_tokens = config.tokenizer(
+        texts,
+        padding=True,
+        truncation=True,
+        max_length=config.pad_size
+    )
     token_ids_list = text_tokens["input_ids"]
     token_attention_mask = text_tokens["attention_mask"]
 
@@ -100,6 +106,53 @@ def collate_fn(batch):
     risk_grad = torch.tensor(risk_grad,dtype=torch.long)
     department = torch.tensor(department,dtype=torch.long)
     return input_ids,attention_mask,fault,risk_grad,department
+
+# 测试集，训练集，验证集拆分
+def dataloader():
+    np.random.seed(42)
+    df= dataset()
+    # print(df)
+    df['combined_label'] = (
+        df['fault'].astype(str) + '_' +
+        df['risk_grad'].astype(str) + '_' +
+        df['department'].astype(str)
+    )
+    # 第一层拆分训练+验证，测试集
+    x_temp,x_test,y_temp,y_test = train_test_split(df.drop(['fault','risk_grad','department','combined_label'],
+                                                axis=1),df[['fault','risk_grad','department','combined_label']],test_size=0.15,
+                                                random_state=42,stratify=df['combined_label'])
+    # 第二层拆分训练，测试集
+    x_train, x_val, y_train, y_val = train_test_split(x_temp,y_temp,test_size=0.1765,
+                                                        random_state=42,stratify=y_temp['combined_label'])
+
+
+    train_dataset = list(zip(
+        x_train['text'].tolist(),
+        y_train['fault'].tolist(),
+        y_train['risk_grad'].tolist(),
+        y_train['department'].tolist()
+    ))
+    val_dataset = list(zip(
+        x_val['text'].tolist(),
+        y_val['fault'].tolist(),
+        y_val['risk_grad'].tolist(),
+        y_val['department'].tolist()
+    ))
+    test_dataset = list(zip(
+        x_test['text'].tolist(),
+        y_test['fault'].tolist(),
+        y_test['risk_grad'].tolist(),
+        y_test['department'].tolist()
+    ))
+
+    train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, collate_fn=collate_fn)
+    val_dataloader = DataLoader(val_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=collate_fn)
+    test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=False, collate_fn=collate_fn)
+    return train_dataloader, val_dataloader, test_dataloader
+
+
+if __name__ == "__main__":
+    dataloader()
 
 
 
